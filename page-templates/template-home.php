77c18dc128ceabda1f12c42e68e50045c51fc4a7
rhysmaddren <rhysmaddren@hotmail.co.uk>
2020-10-06 00:20:36 +0100

    Styled home page navigation

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -17,18 +17,39 @@ get_header();
     <p class="banner-text">Seeing is believing</p>
 </div>
 
-<section class="welcome-section">
-    <h1>Welcome to Simon Berry Optometrist</h1>
-    <p>
-        Thank you for visiting Simon Berry Optometrist, and a 
-        warm welcome to our website. Our professional 
-        independent eyecare practice provides a friendly,
-        customer-focused service to the fantastic people 
-        our local community, but offers a diverse range of
-        services that people from all over the world come 
-        to our practice for. 
-    </p>
-</section>    
+<div class="background-container">
+    <section class="welcome-section">
+        <h1>Welcome to Simon Berry Optometrist</h1>
+        <p>
+            Thank you for visiting Simon Berry Optometrist, and a 
+            warm welcome to our website. Our professional 
+            independent eyecare practice provides a friendly,
+            customer-focused service to the fantastic people 
+            our local community, but offers a diverse range of
+            services that people from all over the world come 
+            to our practice for. 
+        </p>
+    </section>
+
+    <section class="action-container">
+
+        <a href="#" class="call-to-action action-services">
+            <p class="action-text">Services</p>
+        </a>
+
+        <a href="#" class="call-to-action action-repairs">
+            <p class="action-text">Repairs</p>
+        </a>
+        
+        <a href="#" class="call-to-action action-contacts">
+            <p class="action-text">Contacts</p>
+        </a>
+
+    </section>
+    
+
+</div>
+    
 
 
 <?php
